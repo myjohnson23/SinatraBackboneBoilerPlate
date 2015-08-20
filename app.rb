17f590e6e7f2f00c5e5752c715eb7781ad5cc74c
myjohnson23 <myjohnson@gmail.com>
2015-08-20 15:22:10 -0500

    Restful API

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
-  :database => 'testdb1'
+  :database => 'instafake'
 )
 
 get '/' do
@@ -24,4 +24,61 @@ end
 
 get '/4' do
   erb :four
+end
+
+get '/5' do
+  erb :five
+end
+
+get '/6' do
+  erb :six
+end
+
+get '/7' do
+  erb :seven
+end
+
+get '/8' do
+  erb :eight
+end
+
+##### RESTFUL API
+
+## get
+get '/api/instafake' do
+  InstagramModel.all.to_json
+end
+## get by id
+get '/api/instafake/:id' do
+  InstagramModel.find(params[:id]).to_json
+end
+## create
+post '/api/instafake' do
+  InstagramModel.create(params).to_json
+end
+## update
+put '/api/instafake/:id' do
+  @id = params[:id]
+  @insta = InstagramModel.find(@id)
+  @insta.username = params[:username]
+  @insta.post = params[:post]
+  @insta.description = params[:description]
+  @insta.hashtags = params[:hashtags]
+  @insta.save
+  @insta.to_json
+end
+
+patch '/api/instafake/:id' do
+  @id = params[:id]
+  @insta = InstagramModel.find(@id)
+  @insta.username = params[:username]
+  @insta.post = params[:post]
+  @insta.description = params[:description]
+  @insta.hashtags = params[:hashtags]
+  @insta.save
+  @insta.to_json
+end
+## delete
+delete '/api/instafake/:id' do
+  InstagramModel.destroy(params[:id]).to_json
 end
